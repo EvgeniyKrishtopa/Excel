7295ea4a616b597cfa9e0df7d19771b409229967
EvgeniyKrishtopa <evgeniykrishtopa@gmail.com>
2020-08-11 19:21:44 +0300

    finish refactoring code

---
+++ src/core/store/createStore.test.js
@@ -0,0 +1,40 @@
+import { createStore } from "./createStore";
+
+const initialState = {
+  count: 0,
+};
+const reducer = (state = initialState, action) => {
+  if (action.type === "ADD") {
+    return { ...state, count: state.count + 1 };
+  }
+
+  return state;
+};
+
+describe("createStore:", () => {
+  let store;
+
+  beforeEach(() => {
+    store = createStore(reducer, initialState);
+  });
+
+  test("should return store object", () => {
+    expect(store).toBeDefined();
+    expect(store.dispatch).toBeDefined();
+    expect(store.subscribe).toBeDefined();
+    expect(store.getState).toBeDefined();
+  });
+
+  test("should return object as a state", () => {
+    expect(store.getState()).toBeInstanceOf(Object);
+  });
+
+  test("should return default state", () => {
+    expect(store.getState()).toEqual(initialState);
+  });
+
+  test('should change state if action exists', () => {
+    store.dispatch({type: 'ADD'});
+    expect(store.getState().count).toBe(1);
+  })
+});
--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -4,7 +4,7 @@ import { Header } from "@/components/header/Header";
 import { Toolbar } from "@/components/toolbar/Toolbar";
 import { Formula } from "@/components/formula/Formula";
 import { Table } from "@/components/table/Table";
-import { createStore } from "@/core/createStore";
+import { createStore } from "@/core/store/createStore";
 import { rootReducer } from "@/redux/rootReducer";
 import { storage, debounce } from "@/core/utils";
 import { getCurrentDate } from "../core/utils";
@@ -18,15 +18,19 @@ export class ExcelPage extends Page {
     super(params);
     this.params = params;
     this.data = getCurrentDate();
+    this.storeSub = null;
   }
+
   getRoot() {
     const params = this.params ? this.params : this.data;
     const store = createStore(rootReducer, storage(storageName(params)));
+
     const stateListener = debounce(
       (state) => storage(storageName(params), state),
       300
     );
-    store.subscribe(stateListener);
+
+    this.storeSub = store.subscribe(stateListener);
     this.excel = new Excel({
       components: [Header, Toolbar, Formula, Table],
       store,
@@ -41,5 +45,6 @@ export class ExcelPage extends Page {
 
   destroy() {
     this.excel.destroy();
+    this.storeSub = null;
   }
 }
